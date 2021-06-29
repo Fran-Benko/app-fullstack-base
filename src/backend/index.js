194cbeb37a05d654f5ad549308740cf1e78fe551
Franco Benko <francobenko@Francos-MacBook-Air.local>
2021-06-29 11:38:10 -0300

    actualizacion 29 junio

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,38 +1,116 @@
+
 //=======[ Settings, Imports & Data ]==========================================
 
-var PORT    = 3000;
+var PORT = 3000;
 
+var fs = require('fs');
 var express = require('express');
-var app     = express();
-var utils   = require('./mysql-connector');
+var app = express();
+var bodyParser = require('body-parser');
+// var utils = require('./mysql-connector');
+
 
 // to parse application/json
-app.use(express.json()); 
+app.use(express.json());
 // to serve static files
 app.use(express.static('/home/node/app/static/'));
 
-//=======[ Main module code ]==================================================
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+//Ejercicio 3
+var datos = require('./datos.json');
+
+
+
+//Ejercicio 4
+app.get('/devices/', function(req, res) {
+    //res.send(datos);
+    res.json(datos);
 });
 
+//Ejercicio 5
+//Espera una consulta al endpoint EJ /devices/1
+//Parámetro id = el id del dispositivo a buscar
+// devuelve el dispositivo con el id que viene del parametro
+app.get('/devices/:id', function(req, res) {
+    let datosFiltrados = datos.filter(item => item.id == req.params.id);
+
+    res.json(datosFiltrados[0]);
+});
+
+//Ejercicio 6
+//Espera recibir {id:1,state:1/0} , impacta el cambio y lo devuelve
+app.post('/devices/', function(req, res) {
+    let datosFiltrados = datos.filter(item => item.id == req.body.id);
+    if (datosFiltrados.length > 0) {
+        datosFiltrados[0].state = req.body.state;
+    }
+    //res.json(datosFiltrados);
+    res.send("Todo ok");
+});
+
+//Agregar dispositivos
+//Espera recibir la informacion del form del modal, impacta el cambio en datos.
+app.get('/devices_add', function(req, res) {
+    res.sendFile('/home/node/app/static/index.html')
+});
+
+app.post('/devices_add', function(req, res) {
+
+    var newData = {
+        'id':datos.length+1,
+        'name':req.body.nombreDispositivo,
+        'description':req.body.descripcionDispositivo,
+        'state':0,
+        'type':req.body.tipoDispositivo
+    };
+
+    datos.push(newData);
+
+    console.log("Datos Cargados");
+
+    res.sendFile('/home/node/app/static/index.html')
+});
+
+//Eliminar dispositivos
+//Espera recibir la informacion del id del dispositivo, impacta el cambio en datos.
+app.get('/devices/delete/:id', function(req, res) {
+    
+    let id = req.params.id;
+    let datosSinId = datos.filter(item => item.id != id);
+    datos = datosSinId
+
+    let initId = 1;
+    datos.forEach(element => {
+        element.id = initId;
+        initId += 1;
+    });
+
+    console.log('Se elimino el Dispositivo satisfactoriamente el id ', id)
+    console.log(datos)
+    res.send('Se elimino el Dispositivo satisfactoriamente, vuelve a introducir la url inicial para visualizar el cambio.')
+});
+
+//Editar dispositivos
+//Espera recibir la informacion del form del modal, impacta el cambio en datos.
+app.get('/devices/edit/:id', function(req, res) {
+    let datosFiltrados = datos.filter(item => item.id == req.params.id);
+
+    res.json(datosFiltrados[0]);
+});
+
+app.get('/devices_add', function(req, res) {
+    //let name = req.body.name;
+    //let description = req.body.description;
+    //let tipo = req.body.tipo
+
+    res.sendFile('/home/node/app/static/index.html')
+
+});
+
+
+//=======[ Main module code ]==================================================
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
 });
--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,104 @@
+class Main implements EventListenerObject, HandlerPost{
+    public myFramework: MyFramework;
+    
+    
+    
+    public main(): void {
+        console.log("Se ejecuto el metodo main!!!");
+        this.myFramework = new MyFramework();
+        
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        console.log("Llego la respuesta!!!!");
+                        console.log(xhr.responseText);
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+                        let listaDis: Array<Device> = JSON.parse(xhr.responseText);
+                        
+                        for (let disp of listaDis ){
+                        
+                            let listaDisp = this.myFramework.getElementById("listaDisp");
+                            listaDisp.innerHTML += `<li class="collection-item avatar">
+                            <img src="./static/images/lightbulb.png" alt="" class="circle">
+                            <span class="nombreDisp">${disp.name}</span>
+                            <p>${disp.description}
+                            </p>
+                            <a href="#!" class="secondary-content">
+                                <div class="switch">
+                                    <label >
+                                      Off
+                                      <input id="disp_${disp.id}" type="checkbox">
+                                      <span class="lever"></span>
+                                      On
+                                    </label>
+                                  </div>
+                                  <a href="/devices/edit/${disp.id}" class="btn-floating btn-small waves-effect waves-blue btn-edit-device tooltipped" data-position="bottom" data-tooltip="Editar">
+                                    <i id ="edit-${disp.id}" class="small material-icons">edit</i>
+                                  </a>
+                                  <a href="/devices/delete/${disp.id}" class="btn-floating btn-small waves-effect waves-red btn-delete-device tooltipped" data-position="bottom" data-tooltip="Eliminar">
+                                    <i id ="delete-${disp.id}" class="small material-icons">delete</i>
+                                  </a>
+                            </a>
+                          </li>`;
+                         
+                            
+                        }
+
+                        for (let disp of listaDis) {
+                            let checkDisp = this.myFramework.getElementById("disp_" + disp.id);
+                            checkDisp.addEventListener("click", this);
+                        }
+                    } else {
+                        alert("error!!")
+                    }
+                }
+            }
+            xhr.open("GET","http://localhost:8000/devices",true)
+            xhr.send();
+         
+    }
+    
+    public handleEvent(ev: Event) {
+    
+    
+        let objetoClick: HTMLElement = <HTMLElement>ev.target;
+        
+        
+        if (objetoClick.textContent == "+ Agregar") {
+            let modal = this.myFramework.getElementById('myModal');
+            let span = this.myFramework.getElementById("cruzClose");
+            
+            // When the user clicks the button, open the modal 
+            modal.style.display = "block";
+            
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }
+    }
+
+    responsePost(status: number, response: string) {
+        alert(response);
+    }
+
+    
 }
+window.addEventListener("load", ()=> {
+    let miObjMain: Main = new Main();
+    miObjMain.main();
+
+    let boton:HTMLElement = miObjMain.myFramework.getElementById("boton");
+    boton.addEventListener("click", miObjMain);
+    
+    let btnCerrar: HTMLElement = miObjMain.myFramework.getElementById("btnCerrar");
+    btnCerrar.addEventListener("dblclick", miObjMain);
+    
+});
